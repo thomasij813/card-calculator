cd13f30d19466690128ab397f126f9d674bdaab2
Thomas Johnson <thomasij813@gmail.com>
2015-11-10 16:36:37 -0800

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cardCalculatorApp
+ * @description
+ * # cardCalculatorApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cardCalculatorApp', []);
