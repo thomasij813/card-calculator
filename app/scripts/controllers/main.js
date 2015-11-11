109218ffa9a5998f99b46ae057a3ea029ac64fb7
Thomas Johnson <thomasij813@gmail.com>
2015-11-10 16:41:52 -0800

    Clean up boilerplate

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('cardCalculatorApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
   });
